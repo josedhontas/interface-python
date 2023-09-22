4a0b41c64073f948a2f5d2c93b0ada8b1b65d382
josedhonatas <dhonatas@academico.ufs.br>
2023-09-21 23:36:33 -0300

    lk

--- main.py
+++ main.py
@@ -1,37 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from my import *
 
-def imagefft(I):
-    # Calcula a DFT 2D
-    F = np.fft.fft2(I)
-    
-    # Centraliza a DFT
-    F = np.fft.fftshift(F)
-    
-    # Obtém a magnitude (não é estritamente necessário)
-    magnitude = np.abs(F)
-    
-    # Aplica uma escala logarítmica (adiciona 1 para evitar log(0))
-    log_magnitude = np.log(magnitude + 1)
-    
-    # Renormaliza a magnitude para 0-255
-    normalized_magnitude = log_magnitude / np.max(log_magnitude) * 255
-    
-    # Calcula a IDFT 2D (inversa da DFT)
-    reconstructed_image = np.fft.ifft2(np.fft.ifftshift(F)).real
-    
-    # Exibe a magnitude e a imagem reconstruída em tons de cinza
-    plt.subplot(121)
-    plt.imshow(normalized_magnitude, cmap='gray')
-    plt.title('Magnitude da DFT')
-    
-    plt.subplot(122)
-    plt.imshow(reconstructed_image, cmap='gray')
-    plt.title('Imagem Reconstruída em Tons de Cinza')
-    
-    plt.show()
+img = imread("images\lena1.jpg")
+img_uint = (img*255).astype(np.uint8)
+img_negative = 255 - img
+imshow(contrast(img, 1, 200))
 
-# Exemplo de uso:
-img = mpimg.imread("images\lena1.jpg")
-imagefft(img)  # Aplica a função imagefft à imagem em tons de cinza
